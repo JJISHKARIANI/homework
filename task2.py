1221ddf04bbde72cf72051c8e657fe3330d612fd
JJISHKARIANI <j.i.jishkariani@gmail.com>
2026-09-24 11:10:37 +0400

    task2.py

--- task2.py
+++ task2.py
@@ -1,6 +1,10 @@
 n = int(input("Enter a postive integer: "))
 total_sum = 0
 
+if n < 0:
+    print(f"The {n} is not a Positive Integer.)
+    exit()
+    
 for i  in range(2, n + 1, 2):
     total_sum += i
 
